f36ffcc26e8a940fe1417136413bced10893953f
rapon1kt <shinnatensei@gmail.com>
2024-12-31 13:50:20 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- tests/presentation/controllers/signup-controller/signup-controller.spec.ts
+++ tests/presentation/controllers/signup-controller/signup-controller.spec.ts
@@ -24,4 +24,18 @@ describe("SingUp Controller", () => {
 		const httpResponse = sut.handle(httpRequest);
 		expect(httpResponse.statusCode).toBe(400);
 	});
+
+	test("Should return 400 if no email is provided", () => {
+		const sut = new SignUpController();
+		const httpRequest = {
+			body: {
+				name: "any_name",
+				email: "",
+				password: "any_password",
+				passwordConfirmation: "any_password",
+			},
+		};
+		const httpResponse = sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(400);
+	});
 });
