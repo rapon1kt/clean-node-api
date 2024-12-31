a853fc3c47778dcac2854f2bb72c3b0df23f49c9
rapon1kt <shinnatensei@gmail.com>
2024-12-31 16:37:49 -0300

    test: ensure SignUpController calls AddAccount with correct values

--- tests/presentation/controllers/signup-controller/signup-controller.spec.ts
+++ tests/presentation/controllers/signup-controller/signup-controller.spec.ts
@@ -1,3 +1,5 @@
+import { Account } from "@/domain/models";
+import { AddAccount, AddAccountModel } from "@/domain/usecases";
 import { SignUpController } from "@/presentation/controllers/";
 import {
 	InvalidParamError,
@@ -10,6 +12,7 @@ import { describe, expect, test, vi } from "vitest";
 interface sutTypes {
 	sut: SignUpController;
 	emailValidatorStub: EmailValidator;
+	addAccountStub: AddAccount;
 }
 
 const makeEmailValidator = (): EmailValidator => {
@@ -21,12 +24,29 @@ const makeEmailValidator = (): EmailValidator => {
 	return new EmailValidatorStub();
 };
 
+const makeAddAccount = (): AddAccount => {
+	class AddAccountStub implements AddAccount {
+		add(account: AddAccountModel): Account {
+			const fakeAccount = {
+				id: "valid_id",
+				name: "valid_name",
+				email: "valid_email@email.com",
+				password: "valid_password",
+			};
+			return fakeAccount;
+		}
+	}
+	return new AddAccountStub();
+};
+
 const makeSut = (): sutTypes => {
 	const emailValidatorStub = makeEmailValidator();
-	const sut = new SignUpController(emailValidatorStub);
+	const addAccountStub = makeAddAccount();
+	const sut = new SignUpController(emailValidatorStub, addAccountStub);
 	return {
 		sut,
 		emailValidatorStub,
+		addAccountStub,
 	};
 };
 
@@ -172,5 +192,25 @@ describe("SingUp Controller", () => {
 		expect(httpResponse.body).toEqual(
 			new InvalidParamError("passwordConfirmation")
 		);
+	});
+
+	test("Should call AddAccount with correct values", () => {
+		const { sut, addAccountStub } = makeSut();
+		const addSpy = vi.spyOn(addAccountStub, "add");
+		const httpRequest = {
+			body: {
+				name: "any_name",
+				email: "any_email@mail.com",
+				password: "any_password",
+				passwordConfirmation: "any_password",
+			},
+		};
+		sut.sign(httpRequest);
+		expect(addSpy).toHaveBeenCalledWith({
+			id: "192837645123",
+			name: "any_name",
+			email: "any_email@mail.com",
+			password: "any_password",
+		});
 	});
 });
