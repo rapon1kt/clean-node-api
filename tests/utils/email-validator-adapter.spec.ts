bcca8b14dad309ab0c8334a6b09a166e2a919e8f
rapon1kt <shinnatensei@gmail.com>
2025-01-02 18:41:56 -0300

    refactor: implement validator in test file

--- tests/utils/email-validator-adapter.spec.ts
+++ tests/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,20 @@
 import { EmailValidatorAdapter } from "@/utils/email-validator-adapter";
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, vi } from "vitest";
+import validator from "validator";
+
+vi.mock("validator", async (importOriginal) => {
+	return {
+		...(await importOriginal<typeof import("validator")>()),
+		isEmail(): boolean {
+			return true;
+		},
+	};
+});
 
 describe("EmailValidatorAdapter", () => {
 	test("Should return false if validator returns false", () => {
 		const sut = new EmailValidatorAdapter();
+		vi.spyOn(validator, "isEmail").mockReturnValueOnce(false);
 		const isValid = sut.isValid("invalid_email@email.com");
 		expect(isValid).toBe(false);
 	});
