3f841269ec519a859724335f1b251eedc6cdaf2b
rapon1kt <shinnatensei@gmail.com>
2025-01-02 18:42:44 -0300

    test: ensure EmailValidatorAdapter retuns true if validator returns true

--- tests/utils/email-validator-adapter.spec.ts
+++ tests/utils/email-validator-adapter.spec.ts
@@ -18,4 +18,10 @@ describe("EmailValidatorAdapter", () => {
 		const isValid = sut.isValid("invalid_email@email.com");
 		expect(isValid).toBe(false);
 	});
+
+	test("Should return true if validator retuns true", () => {
+		const sut = new EmailValidatorAdapter();
+		const isValid = sut.isValid("valid_email@email.com");
+		expect(isValid).toBe(true);
+	});
 });
