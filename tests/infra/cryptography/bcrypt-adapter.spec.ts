d9d10d2f88538fa384866114b7a81330e1b453e3
rapon1kt <shinnatensei@gmail.com>
2025-01-06 12:29:19 -0300

    test: move sut creation to a factory helper

--- tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -2,10 +2,14 @@ import bcrypt from "bcrypt";
 import { BcryptAdapter } from "@/infra/cryptography";
 import { describe, test, expect, vi } from "vitest";
 
+const salt = 12;
+const makeSut = (): BcryptAdapter => {
+	return new BcryptAdapter(salt);
+};
+
 describe("Bcrypt Adapter", () => {
 	test("Should call bcrypt with correct values", async () => {
-		const salt = 12;
-		const sut = new BcryptAdapter(salt);
+		const sut = makeSut();
 		const hashSpy = vi.spyOn(bcrypt, "hash");
 		await sut.encrypt("any_value");
 		expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
