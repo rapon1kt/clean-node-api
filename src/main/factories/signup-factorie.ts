6a8abaf078e0e5b3881dfb05ba9d619d36ddcd0d
rapon1kt <shinnatensei@gmail.com>
2025-02-18 16:53:15 -0300

    feat(signup): add signup factorie

---
+++ src/main/factories/signup-factorie.ts
@@ -0,0 +1,14 @@
+import { DbAddAccount } from "@/data/usecases";
+import { BcryptAdapter } from "@/infra/cryptography";
+import { AccountMongoRepository } from "@/infra/db/mongodb/account-repository/account-repository";
+import { SignUpController } from "@/presentation/controllers";
+import { EmailValidatorAdapter } from "@/utils";
+
+export const makeSignUpController = (): SignUpController => {
+	const salt = 12;
+	const emailValidatorAdapter = new EmailValidatorAdapter();
+	const bcryptAdapter = new BcryptAdapter(salt);
+	const accountMongoRepository = new AccountMongoRepository();
+	const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository);
+	return new SignUpController(emailValidatorAdapter, dbAddAccount);
+};
