383b27f0061be8d4aa7087a9035662fd5f15ae48
rapon1kt <shinnatensei@gmail.com>
2025-02-27 18:27:43 -0300

    feat: ensure mongodb reconnect if clien is down

--- src/infra/db/mongodb/account-repository/account-repository.ts
+++ src/infra/db/mongodb/account-repository/account-repository.ts
@@ -1,20 +1,16 @@
 import { AddAcccountRepository } from "@/data/protocols";
 import { Account } from "@/domain/models";
 import { AddAccountModel } from "@/domain/usecases";
-import mongoose from "mongoose";
-
-const AccountSchema = new mongoose.Schema<Account>({
-	name: String,
-	email: String,
-	password: String,
-});
+import { MongooseHelper } from "../helpers/mongoose-helper";
 
 export class AccountMongoRepository implements AddAcccountRepository {
 	async add(accountData: AddAccountModel): Promise<Account> {
-		const AccountCollection = mongoose.model("Accounts", AccountSchema);
-		const result = new AccountCollection(accountData);
-		const account = await result.save();
+		const accountCollection = await MongooseHelper.getCollection("accounts");
+		const result = await accountCollection.insertOne(accountData);
+		const account: any = await accountCollection.findOne({
+			_id: result.insertedId,
+		});
 		const { _id, ...accountWithoutId } = account;
-		return Object.assign({}, accountWithoutId, { id: _id });
+		return Object.assign({}, accountWithoutId, { id: account._id });
 	}
 }
--- src/infra/db/mongodb/helpers/mongoose-helper.ts
+++ src/infra/db/mongodb/helpers/mongoose-helper.ts
@@ -2,16 +2,22 @@ import mongoose, { Collection } from "mongoose";
 
 export const MongooseHelper = {
 	client: mongoose,
+	uri: null as string,
 	async connect(uri: string): Promise<void> {
-		this.client = await this.client.connect(uri);
+		this.uri = uri;
+		this.client = await mongoose.connect(uri);
 		await this.client.connection.db.admin().command({ ping: 1 });
 	},
 
 	async disconnect(): Promise<void> {
 		await this.client.disconnect();
+		this.client = null;
 	},
 
-	getCollection(name: string): Collection {
+	async getCollection(name: string): Promise<Collection> {
+		if (this.client.connection.readyState === 0) {
+			await this.connect(this.uri);
+		}
 		return this.client.connection.collection(name);
 	},
 };
