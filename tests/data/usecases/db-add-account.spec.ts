4d83ca97bc07b2b8b86987aea02f670c886d5beb
rapon1kt <shinnatensei@gmail.com>
2025-02-28 11:02:37 -0300

    (db-add-account): remove duplicated code

--- tests/data/usecases/db-add-account.spec.ts
+++ tests/data/usecases/db-add-account.spec.ts
@@ -23,13 +23,7 @@ const makeEncrypter = (): Encrypter => {
 const makeAddAcountRepository = (): AddAcccountRepository => {
 	class AddAccountRepositoryStub implements AddAcccountRepository {
 		add(accountData: AddAccountModel): Promise<Account> {
-			const fakeAccount = {
-				id: "valid_id",
-				name: "valid_name",
-				email: "valid_email",
-				password: "hashed_password",
-			};
-			return new Promise((resolve) => resolve(fakeAccount));
+			return new Promise((resolve) => resolve(makeFakeAccount()));
 		}
 	}
 	return new AddAccountRepositoryStub();
@@ -47,16 +41,24 @@ const makeSut = (): SutTypes => {
 	};
 };
 
+const makeFakeAccount = (): Account => ({
+	id: "valid_id",
+	name: "valid_name",
+	email: "valid_email",
+	password: "hashed_password",
+});
+
+const makeFakeAccountData = (): AddAccountModel => ({
+	name: "valid_name",
+	email: "valid_email",
+	password: "valid_password",
+});
+
 describe("DbAddAccount", () => {
 	test("Should call Encrypter with correct password", async () => {
 		const { sut, encrypterStub } = makeSut();
 		const encryptSpy = vi.spyOn(encrypterStub, "encrypt");
-		const accountData = {
-			name: "valid_name",
-			email: "valid_email",
-			password: "valid_password",
-		};
-		await sut.add(accountData);
+		await sut.add(makeFakeAccountData());
 		expect(encryptSpy).toHaveBeenCalledWith("valid_password");
 	});
 
@@ -65,24 +67,14 @@ describe("DbAddAccount", () => {
 		vi.spyOn(encrypterStub, "encrypt").mockReturnValueOnce(
 			new Promise((resolve, reject) => reject(new Error()))
 		);
-		const accountData = {
-			name: "valid_name",
-			email: "valid_email",
-			password: "invalid_password",
-		};
-		const promise = sut.add(accountData);
+		const promise = sut.add(makeFakeAccountData());
 		await expect(promise).rejects.toThrow();
 	});
 
 	test("Should calls AddAccountRepository with correct values", async () => {
 		const { sut, addAccountRepositoryStub } = makeSut();
 		const addAccountRepositorySpy = vi.spyOn(addAccountRepositoryStub, "add");
-		const accountData = {
-			name: "valid_name",
-			email: "valid_email",
-			password: "valid_password",
-		};
-		await sut.add(accountData);
+		await sut.add(makeFakeAccountData());
 		expect(addAccountRepositorySpy).toHaveBeenCalledWith({
 			name: "valid_name",
 			email: "valid_email",
@@ -95,28 +87,13 @@ describe("DbAddAccount", () => {
 		vi.spyOn(addAccountRepositoryStub, "add").mockReturnValueOnce(
 			new Promise((resolve, reject) => reject(new Error()))
 		);
-		const accountData = {
-			name: "valid_name",
-			email: "valid_email",
-			password: "valid_password",
-		};
-		const promise = sut.add(accountData);
+		const promise = sut.add(makeFakeAccountData());
 		await expect(promise).rejects.toThrow();
 	});
 
 	test("Should return an account on success", async () => {
 		const { sut } = makeSut();
-		const accountData = {
-			name: "valid_name",
-			email: "valid_email",
-			password: "valid_password",
-		};
-		const account = await sut.add(accountData);
-		expect(account).toEqual({
-			id: "valid_id",
-			name: "valid_name",
-			email: "valid_email",
-			password: "hashed_password",
-		});
+		const account = await sut.add(makeFakeAccountData());
+		expect(account).toEqual(makeFakeAccount());
 	});
 });
