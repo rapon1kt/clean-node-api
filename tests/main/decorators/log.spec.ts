983abf7eb614d62bbe3561cb34c6f1b7d183c4dd
rapon1kt <shinnatensei@gmail.com>
2025-02-28 10:32:58 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- tests/main/decorators/log.spec.ts
+++ tests/main/decorators/log.spec.ts
@@ -5,10 +5,13 @@ import {
 	HttpRequest,
 	HttpResponse,
 } from "@/presentation/protocols";
+import { LogErrorRepository } from "@/data/protocols/log-error-repository";
+import { serverError } from "@/presentation/helpers/http-helper";
 
 interface SutTypes {
 	sut: LogControllerDecorator;
 	controllerStub: Controller;
+	logErrorRepositoryStub: LogErrorRepository;
 }
 
 const makeController = (): Controller => {
@@ -26,12 +29,26 @@ const makeController = (): Controller => {
 	return new ControllerStub();
 };
 
+const makeLogErrorRepositoryStub = (): LogErrorRepository => {
+	class LogErrorRepositoryStub implements LogErrorRepositoryStub {
+		async log(stack: string): Promise<void> {
+			return new Promise((resolve) => resolve());
+		}
+	}
+	return new LogErrorRepositoryStub();
+};
+
 const makeSut = (): SutTypes => {
 	const controllerStub = makeController();
-	const sut = new LogControllerDecorator(controllerStub);
+	const logErrorRepositoryStub = makeLogErrorRepositoryStub();
+	const sut = new LogControllerDecorator(
+		controllerStub,
+		logErrorRepositoryStub
+	);
 	return {
 		sut,
 		controllerStub,
+		logErrorRepositoryStub,
 	};
 };
 
@@ -69,5 +86,26 @@ describe("LogController Decorator", () => {
 				name: "Rodrigo",
 			},
 		});
+	});
+
+	test("Should call LogRepository with correct error if controller returns a server error", async () => {
+		const { sut, controllerStub, logErrorRepositoryStub } = makeSut();
+		const fakeError = new Error();
+		fakeError.stack = "any_stack";
+		const error = serverError(fakeError);
+		const logSpy = vi.spyOn(logErrorRepositoryStub, "log");
+		vi.spyOn(controllerStub, "handle").mockReturnValueOnce(
+			new Promise((resolve) => resolve(error))
+		);
+		const httpRequest = {
+			body: {
+				email: "any_mail@mail.com",
+				name: "any_name",
+				password: "any_password",
+				passwordConfirmation: "any_password",
+			},
+		};
+		await sut.handle(httpRequest);
+		expect(logSpy).toHaveBeenCalledWith("any_stack");
 	});
 });
